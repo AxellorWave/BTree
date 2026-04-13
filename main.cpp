f1d2ab35f9a01efbf46507e47fbda005b3be953e
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-04-13 22:19:34 +0300

    added signature

--- main.cpp
+++ main.cpp
@@ -1,2 +1,38 @@
+#include <iostream>
+
+template< class T, size_t K >
+struct BTree {
+  T val[K];
+  BTree< T, K > * childs[K + 1];
+  BTree< T, K > * parent;
+};
+
+template< class T, size_t K >
+struct BTreeIt {
+  size_t s;
+  BTree< T, K > * current;
+};
+
+template< class T, size_t K >
+T value(BTreeIt< T, K > it);
+
+template< class T, size_t K >
+BTree< T, K > * minimum(BTree< T, K > * root);
+
+template< class T, size_t K >
+BTree< T, K > * maximum(BTree< T, K > * root);
+
+template< class T, size_t K >
+BTreeIt< T, K > next(BTreeIt< T, K > it);
+
+template< class T, size_t K >
+BTreeIt< T, K > prev(BTreeIt< T, K > it);
+
+template< class T, size_t K >
+bool hasNext(BTreeIt< T, K > it);
+
+template< class T, size_t K >
+bool hasNext(BTreeIt< T, K > it);
+
 int main()
 {}
